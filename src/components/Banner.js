da58dd152c9fff237e3237b97a57df1160752c62
asam <abbasalimokolo@gmail.com>
2020-09-11 21:46:55 +0100

    Font

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 const Banner = props => {
     return(
         <section className="h-48 bg-red-700 w-full flex items-center justify-center">
-            <h1 className="text-white text-3xl font-semibold">{props.title}</h1>
+            <h1 className="text-white text-3xl font-black">{props.title}</h1>
         </section>
     )
 }
